129f641d1a951de8f1e1747fb17e8f48d2ba1925
VitorMaverick <vitor.maverick@bemobi.com>
2024-09-12 21:01:40 -0300

    Adicionando migrations

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,11 +1,11 @@
+import { env } from '@/env'
 import { defineConfig } from 'drizzle-kit'
-import { env } from './src/env'
 
 export default defineConfig({
-  dialect: 'postgresql', // "mysql" | "sqlite" | "postgresql"
-  schema: './src/db/schema.ts',
-  out: '././migrations',
+  schema: './src/db/schema/index.ts',
+  out: './.migrations',
   dbCredentials: {
     url: env.DATABASE_URL,
   },
+  dialect: 'postgresql',
 })
---
+++ server/src/db/index.ts
@@ -0,0 +1,7 @@
+import { env } from '@/env'
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+import * as schema from './schema'
+
+export const client = postgres("postgresql://docker:docker@localhost:5432/inorbit")
+export const db = drizzle(client, { schema, logger: true })
